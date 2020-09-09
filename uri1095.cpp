09f228e3e454b052421e2232240733637dbd4760
nafejul75 <68812446+nafejul75@users.noreply.github.com>
2020-09-09 22:06:02 +0600

    Create uri1095.cpp

---
+++ uri1095.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+     int i=1,j=60;
+     while(j>=0) {
+        printf("I=%d J=%d\n",i,j);
+        i+=3,j-=5;
+    }
+ 
+    return 0;
+}
